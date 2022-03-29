01153127861a2fa40d851ec2137b987fc219f17a
araderma <annika.radermacher@bam.de>
2022-03-29 10:06:17 +0200

    set up workflow

---
+++ tests/unit/test_autotest.py
@@ -0,0 +1,17 @@
+'''
+    test continuous integration test enviroment
+'''
+
+import unittest
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.a = 2
+
+
+    def test_a(self):
+        self.assertAlmostEqual(2, self.a, places=7)
+
+
+if __name__ == "__main__":
+    unittest.main()
