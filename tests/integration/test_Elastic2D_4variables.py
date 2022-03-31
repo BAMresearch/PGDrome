71778683f6e3c2742621ffd80978a1a12e9a545d
Aratz Garcia-Llona <68382462+agarllo@users.noreply.github.com>
2022-03-31 14:56:36 +0200

    Update test_Elastic2D_4variables.py

--- tests/integration/test_Elastic2D_4variables.py
+++ tests/integration/test_Elastic2D_4variables.py
@@ -5,17 +5,12 @@ computed by the two mehods are compared to check its working well.
 
 """
 
-# import fenics as fe
 import unittest
 from dolfin import *
-#import matplotlib.pyplot as plt
 import numpy as np
 import os
 from pgdrome.solver import PGDProblem1
 
-###############################################################################
-################################     INPUT     ################################
-###############################################################################
 
 # MESHES
 #==============================================================================
@@ -39,7 +34,7 @@ def create_meshes(mesh_0,V_X,input_mesh):
             aux_V = FunctionSpace(aux_mesh,input_mesh[k][2],input_mesh[k][3])
             meshes.append(aux_mesh) # Get out of the IF when k==0 is written
             Vs.append(aux_V) # Get out of the IF when k==0 is written
-    
+        
         else:
             print("Error: Not well defined")
             
@@ -97,7 +92,7 @@ def problem_assemble_lhs(fct_F,var_F,Fs,meshes,dom,param,typ,dim):
     if typ == 'r':
         a = Constant(assemble(Fs[1] * Fs[1] * dx(meshes[1])) \
             * assemble(Fs[2] * param["Efunc"] * Fs[2] * dx(meshes[2])) \
-            * assemble(Fs[3] * param["nufunc"][0] * Fs[3] * dx(meshes[3])) )\
+            * assemble(Fs[3] * param["nufunc"][0] * Fs[3] * dx(meshes[3]))) \
             * inner(param["aux_C"][0]*epsilon(fct_F),epsilon(var_F)) * dx(meshes[0]) \
             + Constant(assemble(Fs[1] * Fs[1] * dx(meshes[1])) \
             * assemble(Fs[2] * param["Efunc"] * Fs[2] * dx(meshes[2])) \
@@ -108,7 +103,7 @@ def problem_assemble_lhs(fct_F,var_F,Fs,meshes,dom,param,typ,dim):
     if typ == 's':
         a = Constant(assemble(inner(param["aux_C"][0]*epsilon(Fs[0]),epsilon(Fs[0])) * dx(meshes[0])) \
             * assemble(Fs[2] * param["Efunc"] * Fs[2] * dx(meshes[2])) \
-            * assemble(Fs[3] * param["nufunc"][0] * Fs[3] * dx(meshes[3])) ) \
+            * assemble(Fs[3] * param["nufunc"][0] * Fs[3] * dx(meshes[3])) )\
             * var_F* fct_F * dx(meshes[1]) \
             + Constant(assemble(inner(param["aux_C"][1]*epsilon(Fs[0]),epsilon(Fs[0])) * dx(meshes[0])) \
             * assemble(Fs[2] * param["Efunc"] * Fs[2] * dx(meshes[2])) \
@@ -150,7 +145,7 @@ def problem_assemble_rhs(fct_F, var_F, Fs, meshes, dom, param, G, PGD_func, typ,
     # Computing R assuming S, T and V is known: 
     if typ == 'r':
         # for ext in range(len(G[0][0])):
-        l += Constant(assemble(Fs[1] * dx(meshes[1])) \
+        l += Constant(assemble(param["Afunc"] * Fs[1] * dx(meshes[1])) \
             * assemble(Fs[2] * dx(meshes[2])) \
             * assemble(Fs[3] * dx(meshes[3])) )\
             * dot(G[0][0][0],var_F) * ds
@@ -163,7 +158,7 @@ def problem_assemble_rhs(fct_F, var_F, Fs, meshes, dom, param, G, PGD_func, typ,
                     * inner(param["aux_C"][0]*epsilon(PGD_func[0][old]),epsilon(var_F)) * dx(meshes[0]) \
                     - Constant(assemble(Fs[1] * PGD_func[1][old] * dx(meshes[1])) \
                     * assemble(Fs[2] * param["Efunc"] * PGD_func[2][old] * dx(meshes[2])) \
-                    * assemble(Fs[3] * param["nufunc"][1] * PGD_func[3][old] * dx(meshes[3]))) \
+                    * assemble(Fs[3] * param["nufunc"][1] * PGD_func[3][old] * dx(meshes[3])) )\
                     * inner(param["aux_C"][1]*epsilon(PGD_func[0][old]),epsilon(var_F)) * dx(meshes[0])
                         
     # Computing S assuming R, T and V is known:
@@ -171,8 +166,8 @@ def problem_assemble_rhs(fct_F, var_F, Fs, meshes, dom, param, G, PGD_func, typ,
         # for ext in range(len(G[0][1])):
         l += Constant(assemble(dot(G[0][0][0],Fs[0]) * ds) \
             * assemble(Fs[2] * dx(meshes[2])) \
-            * assemble(Fs[3] * dx(meshes[3]))) \
-            * var_F * dx(meshes[1])
+            * assemble(Fs[3] * dx(meshes[3])) )\
+            * var_F * param["Afunc"] * dx(meshes[1])
 
         if nE > 0:
             for old in range(nE):
@@ -190,7 +185,7 @@ def problem_assemble_rhs(fct_F, var_F, Fs, meshes, dom, param, G, PGD_func, typ,
     if typ == 't':
         # for ext in range(len(G[0][1])):
         l += Constant(assemble(dot(G[0][0][0],Fs[0]) * ds) \
-            * assemble(Fs[1] * dx(meshes[1])) \
+            * assemble(param["Afunc"] * Fs[1] * dx(meshes[1])) \
             * assemble(Fs[3] * dx(meshes[3])) )\
             * var_F * dx(meshes[2])
 
@@ -202,14 +197,14 @@ def problem_assemble_rhs(fct_F, var_F, Fs, meshes, dom, param, G, PGD_func, typ,
                     * var_F * param["Efunc"] * PGD_func[2][old] * dx(meshes[2]) \
                     - Constant(assemble(inner(param["aux_C"][1]*epsilon(PGD_func[0][old]),epsilon(Fs[0])) * dx(meshes[0])) \
                     * assemble(Fs[1]*PGD_func[1][old] *dx(meshes[1])) \
-                    * assemble(Fs[3] * param["nufunc"][1] * PGD_func[3][old] * dx(meshes[3])) ) \
+                    * assemble(Fs[3] * param["nufunc"][1] * PGD_func[3][old] * dx(meshes[3])) )\
                     * var_F * param["Efunc"] * PGD_func[2][old] * dx(meshes[2])
 
     # Computing V assuming R, S and T is known:
     if typ == 'v':
         # for ext in range(len(G[0][1])):
         l += Constant(assemble(dot(G[0][0][0],Fs[0]) * ds) \
-            * assemble(Fs[1] * dx(meshes[1])) \
+            * assemble(param["Afunc"] * Fs[1] * dx(meshes[1])) \
             * assemble(Fs[2] * dx(meshes[2])) )\
             * var_F * dx(meshes[3])
 
@@ -250,7 +245,8 @@ def main(Vs):
     coef_2 = Expression("1./(2. * (1.0 +x[0]))", degree=1)
     coef = [coef_1, coef_2]
     
-    param = {"aux_C": aux_C, "nufunc": coef, "Efunc":Expression('x[0]', degree=4)} # Define parameteres
+    param = {"aux_C": aux_C, "nufunc": coef, "Efunc":Expression('x[0]', degree=4),\
+             "Afunc":Expression('x[0]', degree=4)} # Define parameteres
     
     # Define BC:
     #-----------------------------
@@ -276,8 +272,7 @@ def main(Vs):
     # Solve the problem
     #-----------------------------
     pgd_prob.stop_fp='norm'
-    # pgd_prob.max_fp_it = 5
-    pgd_prob.tol_fp_it = 1e-1
+    pgd_prob.tol_fp_it=1e-1
     pgd_prob.tol_abs = 1e-3
     pgd_prob.solve_PGD(_problem='linear')
     
@@ -311,8 +306,8 @@ class PGDproblem(unittest.TestCase):
 
         # [[X1min, X1max, X2min,  X2max,...],[ne1, ne2,...], 'Family', degree]
         mesh_v1 = [[0., 2.],[10],'Lagrange', 1] # Variable 1: Loading amplitude
-        mesh_v2 = [[0.5, 1.5],[10],'Lagrange', 1] # Variable 2: Young's modulus
-        mesh_v3 = [[0.1, 0.4],[10],'Lagrange', 1] # Variable 3: Poisson's ratio
+        mesh_v2 = [[0.5, 1.5],[50],'Lagrange', 1] # Variable 2: Young's modulus
+        mesh_v3 = [[0.1, 0.4],[50],'Lagrange', 1] # Variable 3: Poisson's ratio
         input_mesh = [mesh_geo,mesh_v1,mesh_v2,mesh_v3] # Grouping meshes
         
         meshes, Vs = create_meshes(mesh_0,V_X,input_mesh)
@@ -354,10 +349,9 @@ class PGDproblem(unittest.TestCase):
         # Solve:
         u = Function(Vs[0])
         solve(rhs==lhs,u,bc[0])
-        errorL2 = np.linalg.norm(u_pgd.vector()[:]-u.vector()[:],2)
-        print('error',errorL2)
-        
-        self.assertTrue(errorL2<0.01)
+        errorL2 = np.linalg.norm(u_pgd.vector()[:]-u.vector()[:],2)/np.linalg.norm(u.vector()[:],2)
+        # print(errorL2)
+        self.assertTrue(errorL2<0.03)
 
 if __name__ == '__main__':
 
