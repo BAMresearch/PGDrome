f0ca89873812ad1d59023e62cb8cbb3c3ba1522f
araderma <annika.robens-radermacher@bma.de>
2022-10-07 14:03:43 +0200

    fixed dimless version

--- tests/integration/test_convection2D.py
+++ tests/integration/test_convection2D.py
@@ -27,7 +27,127 @@ def create_meshes(num_elem, ord, ranges):
         meshes.append(mesh_tmp)
         Vs.append(Vs_tmp)
 
-    return meshes, Vs
+    return meshes, Vs #x,y,t,h
+
+def create_bc(Vs,dom,param):
+    # boundary conditions list
+
+    # Initial condition
+    def init(x, on_boundary):
+        return x < 0.0 + 1E-5
+
+    initCond = dolfin.DirichletBC(Vs[1], 0, init)
+
+    return [0, 0, initCond, 0] #x,y,t,h
+
+
+def problem_assemble_lhs_FDtime(fct_F,var_F,Fs,meshes,dom,param,typ,dim):
+    # problem discription left hand side of DGL for each fixed point problem
+
+    if typ == 'r':
+        alpha_1 = Fs[1].vector()[:].transpose() @ param['D1_up_t'] @ Fs[1].vector()[:]
+        alpha_2 = Fs[1].vector()[:].transpose() @ param['M_t'] @ Fs[1].vector()[:]
+        a = dolfin.Constant(alpha_1 \
+            * dolfin.assemble(Fs[2] * Fs[2] * dolfin.dx(meshes[2]))) \
+            * param['a1'] * param["rho"] * param["cp"] * fct_F * var_F * dolfin.dx(meshes[0]) \
+            + dolfin.Constant(alpha_2 \
+            * dolfin.assemble(Fs[2] * Fs[2] * dolfin.dx(meshes[2]))) \
+            * param['a2'] * param["k"] * fct_F.dx(0) * var_F.dx(0) * dolfin.dx(meshes[0])
+    if typ == 's':
+        a = dolfin.assemble(Fs[0] * Fs[0] * dolfin.dx(meshes[0])) \
+            * dolfin.assemble(Fs[2] * Fs[2] * dolfin.dx(meshes[2])) \
+            * param['a1'] * param["rho"] * param["cp"] * param['D1_up_t'] \
+            + dolfin.assemble(Fs[0].dx(0) * Fs[0].dx(0) * dolfin.dx(meshes[0])) \
+            * dolfin.assemble(Fs[2] * Fs[2] * dolfin.dx(meshes[2])) \
+            * param['a2'] * param["k"] * param['M_t']
+        # add initial condition
+        a[:, param['bc_idx']] = 0
+        a[param['bc_idx'], :] = 0
+        a[param['bc_idx'], param['bc_idx']] = 1
+    if typ == 'w':
+        alpha_1 = Fs[1].vector()[:].transpose() @ param['D1_up_t'] @ Fs[1].vector()[:]
+        alpha_2 = Fs[1].vector()[:].transpose() @ param['M_t'] @ Fs[1].vector()[:]
+        a = dolfin.Constant(dolfin.assemble(Fs[0] * Fs[0] * dolfin.dx(meshes[0])) \
+            * alpha_1) \
+            * param['a1'] * param["rho"] * param["cp"] * fct_F * var_F * dolfin.dx(meshes[2])\
+            + dolfin.Constant(dolfin.assemble(Fs[0].dx(0) * Fs[0].dx(0) * dolfin.dx(meshes[0])) \
+            * alpha_2) \
+            * param['a2'] * param["k"] * fct_F * var_F * dolfin.dx(meshes[2])
+    return a
+
+def problem_assemble_rhs_FDtime(fct_F,var_F,Fs,meshes,dom,param,Q,PGD_func,typ,nE,dim):
+    # problem discription right hand side of DGL for each fixed point problem
+
+    IC = [param["IC_x"], param["IC_t"], param["IC_q"]]
+
+    if typ == 'r':
+        betha_1 = Fs[1].vector()[:].transpose() @ param['M_t'] @ Q[1].vector()[:]
+        alpha_1 = Fs[1].vector()[:].transpose() @ param['D1_up_t'] @ IC[1].vector()[:]
+        alpha_2 = Fs[1].vector()[:].transpose() @ param['M_t'] @ IC[1].vector()[:]
+        l = dolfin.Constant(betha_1 \
+            * dolfin.assemble(Q[2] * Fs[2] * dolfin.dx(meshes[2]))) \
+            *  param['b'] * Q[0] * var_F * dolfin.dx(meshes[0]) \
+            - dolfin.Constant(alpha_1 \
+            * dolfin.assemble(IC[2] * Fs[2] * dolfin.dx(meshes[2]))) \
+            * param['a1'] * param["rho"] * param["cp"] * IC[0] * var_F * dolfin.dx(meshes[0]) \
+            - dolfin.Constant(alpha_2 \
+            * dolfin.assemble(IC[2] * Fs[2] * dolfin.dx(meshes[2]))) \
+            * param['a2'] * param["k"] * IC[0].dx(0) * var_F.dx(0) * dolfin.dx(meshes[0])
+        if nE > 0:
+            for old in range(nE):
+                alpha_old_1 = Fs[1].vector()[:].transpose() @ param['D1_up_t'] @ PGD_func[1][old].vector()[:]
+                alpha_old_2 = Fs[1].vector()[:].transpose() @ param['M_t'] @ PGD_func[1][old].vector()[:]
+                l +=- dolfin.Constant(alpha_old_1 \
+                    * dolfin.assemble(PGD_func[2][old] * Fs[2] * dolfin.dx(meshes[2]))) \
+                    * param['a1'] * param["rho"] * param["cp"] * PGD_func[0][old] * var_F * dolfin.dx(meshes[0]) \
+                    - dolfin.Constant(alpha_old_2 \
+                    * dolfin.assemble(PGD_func[2][old] * Fs[2] * dolfin.dx(meshes[2]))) \
+                    * param['a2'] * param["k"] * PGD_func[0][old].dx(0) * var_F.dx(0) * dolfin.dx(meshes[0])
+    if typ == 's':
+        l = dolfin.assemble(Q[0] * Fs[0] * dolfin.dx(meshes[0])) \
+            * dolfin.assemble(Q[2] * Fs[2] * dolfin.dx(meshes[2])) \
+            * param['b'] * param['M_t'] @ Q[1].vector()[:] \
+            - dolfin.assemble(IC[0] * Fs[0] * dolfin.dx(meshes[0])) \
+            * dolfin.assemble(IC[2] * Fs[2] * dolfin.dx(meshes[2])) \
+            * param['a1'] * param["rho"] * param["cp"] * param['D1_up_t'] @ IC[1].vector()[:] \
+            - dolfin.assemble(IC[0].dx(0) * Fs[0].dx(0) * dolfin.dx(meshes[0])) \
+            * dolfin.assemble(IC[2] * Fs[2] * dolfin.dx(meshes[2])) \
+            * param['a2'] * param["k"] * param['M_t'] @ IC[1].vector()[:]
+        if nE > 0:
+            for old in range(nE):
+                l +=- dolfin.assemble(PGD_func[0][old] * Fs[0] * dolfin.dx(meshes[0])) \
+                    * dolfin.assemble(PGD_func[2][old] * Fs[2] * dolfin.dx(meshes[2])) \
+                    * param['a1'] * param["rho"] * param["cp"] * param['D1_up_t'] @ PGD_func[1][old].vector()[:] \
+                    - dolfin.assemble(PGD_func[0][old].dx(0) * Fs[0].dx(0) * dolfin.dx(meshes[0])) \
+                    * dolfin.assemble(PGD_func[2][old] * Fs[2] * dolfin.dx(meshes[2])) \
+                    * param['a2'] * param["k"] * param['M_t'] @ PGD_func[1][old].vector()[:]
+        # add initial condition
+        l[param['bc_idx']] = 0
+    if typ == 'w':
+        betha_1 = Fs[1].vector()[:].transpose() @ param['M_t'] @ Q[1].vector()[:]
+        alpha_1 = Fs[1].vector()[:].transpose() @ param['D1_up_t'] @ IC[1].vector()[:]
+        alpha_2 = Fs[1].vector()[:].transpose() @ param['M_t'] @ IC[1].vector()[:]
+        l = dolfin.Constant(dolfin.assemble(Q[0] * Fs[0] * dolfin.dx(meshes[0])) \
+            * betha_1 ) \
+            * param['b'] * Q[2] * var_F * dolfin.dx(meshes[2]) \
+            - dolfin.Constant(dolfin.assemble(IC[0] * Fs[0] * dolfin.dx(meshes[0])) \
+            * alpha_1) \
+            * param['a1'] * param["rho"] * param["cp"] * IC[2] * var_F * dolfin.dx(meshes[2]) \
+            - dolfin.Constant(dolfin.assemble(IC[0].dx(0) * Fs[0].dx(0) * dolfin.dx(meshes[0])) \
+            * alpha_2) \
+            * param['a2'] * param["k"] * IC[2] * var_F * dolfin.dx(meshes[2])
+        if nE > 0:
+            for old in range(nE):
+                alpha_old_1 = Fs[1].vector()[:].transpose() @ param['D1_up_t'] @ PGD_func[1][old].vector()[:]
+                alpha_old_2 = Fs[1].vector()[:].transpose() @ param['M_t'] @ PGD_func[1][old].vector()[:]
+                l +=- dolfin.Constant(dolfin.assemble(PGD_func[0][old] * Fs[0] * dolfin.dx(meshes[0])) \
+                    * alpha_old_1) \
+                    * param['a1'] * param["rho"] * param["cp"] * PGD_func[2][old] * var_F * dolfin.dx(meshes[2]) \
+                    - dolfin.Constant(dolfin.assemble(PGD_func[0][old].dx(0) * Fs[0].dx(0) * dolfin.dx(meshes[0])) \
+                    * alpha_old_2) \
+                    * param['a2'] * param["k"] * PGD_func[2][old] * var_F * dolfin.dx(meshes[2])
+    return l
+
 
 def create_doms_fem(v,param,fact_dimless):
     boundary = dolfin.MeshFunction("size_t", v.mesh(), v.mesh().topology().dim()-1)
@@ -39,7 +159,7 @@ def create_doms_fem(v,param,fact_dimless):
 
     class Top(dolfin.SubDomain):
         def inside(self, x, on_boundary):
-            return on_boundary and dolfin.near(x[1], param['ly']/fact_dimless['y_0'], dolfin.DOLFIN_EPS)
+            return on_boundary and dolfin.near(x[1], param['ly']/fact_dimless['L_0'], dolfin.DOLFIN_EPS)
 
     class Left(dolfin.SubDomain):
         def inside(self, x, on_boundary):
@@ -47,7 +167,7 @@ def create_doms_fem(v,param,fact_dimless):
 
     class Right(dolfin.SubDomain):
         def inside(self, x, on_boundary):
-            return on_boundary and dolfin.near(x[0], param['lx']/fact_dimless['x_0'], dolfin.DOLFIN_EPS)
+            return on_boundary and dolfin.near(x[0], param['lx']/fact_dimless['L_0'], dolfin.DOLFIN_EPS)
 
     bottom = Bottom()
     bottom.mark(boundary, 1)  # marks bottom of the domain as 1
@@ -80,24 +200,14 @@ class Reference:
         v = dolfin.TestFunction(self.vs[0])
         self.dt = dolfin.Constant(1.)
         self.h = dolfin.Constant(1.)
-        # splitted dot(grad,grad) Term for dimless
+
         self.F = self.param['a1'] * self.param["rho"] * self.param["cp"] * T * v * dolfin.dx() \
-                 + self.dt * self.param['ax'] * self.param["k"] * T.dx(0) * v.dx(0) * dolfin.dx() + self.dt * self.param['ay'] * self.param["k"] * T.dx(1) * v.dx(1) * dolfin.dx() \
-                 + self.dt * self.param['hy'] * self.h * (T - self.T_amb) * v * ds(2) \
-                 + self.dt * self.param['hx'] * self.h * (T - self.T_amb) * v * ds(4) \
+                 + self.param['a2'] * self.dt * self.param["k"] *  dolfin.dot(dolfin.grad(T), dolfin.grad(v)) * dolfin.dx() \
+                 + self.dt * self.param['h1'] * self.h * (T - self.T_amb) * v * ds(2) \
+                 + self.dt * self.param['h1'] * self.h * (T - self.T_amb) * v * ds(4) \
                  - self.param['a1'] * self.param["rho"] * self.param["cp"] * self.T_n * v * dolfin.dx()
-                # + self.dt * self.param['hy'] * self.h * (T - self.T_amb) * v * ds(1) \
-                # + self.dt * self.param['hx'] * self.h * (T - self.T_amb) * v * ds(3) \ # more convection boundaries
-
-        # without split
-        # self.F = self.param["rho"] * self.param["cp"] * T * v * dolfin.dx() \
-        #          + self.dt * self.param["k"] *  dolfin.dot(dolfin.grad(T), dolfin.grad(v)) * dolfin.dx() \
-        #          + self.dt * self.h * (T - self.T_amb) * v * ds(2) \
-        #          + self.dt * self.h * (T - self.T_amb) * v * ds(4) \
-        #          - self.param["rho"] * self.param["cp"] * self.T_n * v * dolfin.dx()
-        #        # + self.dt * self.h * (T - self.T_amb) * v * ds(1) \
-        #        # + self.dt * self.h * (T - self.T_amb) * v * ds(3) \ # more convection boundaries
-
+                   # + self.dt * self.param['h1'] * self.h * (T - self.T_amb) * v * ds(1) \
+                   # + self.dt * self.param['h1'] * self.h * (T - self.T_amb) * v * ds(3) \ # more convection boundaries
 
         self.point = point
 
@@ -142,29 +252,27 @@ class problem(unittest.TestCase):
                       'lx': 0.15, 'ly': 0.1, 'lt': 100}
 
         # possiblity to make the equation dimless
-        # self.factors_o = {'x_0': 0.15, 'y_0': 0.1, 't_0': 100., 'T_0': 500.}
-        self.factors_o = {'x_0': 1, 'y_0': 1, 't_0': 1., 'T_0': 1.}
+        self.factors_o = {'L_0': 0.1, 't_0': 100., 'T_0': 500.}
+        # self.factors_o = {'L_0': 1, 't_0': 1., 'T_0': 1.}
 
         # factors for pde
         self.param['a1'] = self.factors_o['T_0'] / self.factors_o['t_0']
-        self.param['ax'] = self.factors_o['T_0'] / self.factors_o['x_0'] ** 2
-        self.param['ay'] = self.factors_o['T_0'] / self.factors_o['y_0'] ** 2
-        self.param['hx'] = self.factors_o['T_0'] / self.factors_o['x_0']
-        self.param['hy'] = self.factors_o['T_0'] / self.factors_o['y_0']
+        self.param['a2'] = self.factors_o['T_0'] / self.factors_o['L_0'] ** 2
+        self.param['h1'] = self.factors_o['T_0'] / self.factors_o['L_0']
 
-        self.ranges = [[0., self.param['lx']/ self.factors_o['x_0']],  # xmin, xmax
-                       [0., self.param['ly']/ self.factors_o['y_0']],  # ymin, ymax
+        self.ranges = [[0., self.param['lx']/ self.factors_o['L_0']],  # xmin, xmax
+                       [0., self.param['ly']/ self.factors_o['L_0']],  # ymin, ymax
                        [0., self.param['lt']/ self.factors_o['t_0']],  # tmin, tmax
                        [2, 200]]  # hmin, hmax
 
         self.ords = [1, 1, 1, 1]  # x, y, t, h
-        self.elems = [10, 10, 20, 50]
+        self.elems = [20, 10, 20, 50]
 
         self.h_fixed = 200
-        self.point = (self.param['lx'] / self.factors_o['x_0'], self.param['ly']/(2*self.factors_o['y_0']))
+        self.point = (self.param['lx'] / self.factors_o['L_0'], self.param['ly']/(2*self.factors_o['L_0']))
 
-        # self.plotting = True
-        self.plotting = False
+        self.plotting = True
+        # self.plotting = False
 
     def TearDown(self):
         pass
