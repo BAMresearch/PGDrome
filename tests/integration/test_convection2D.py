1f678d51a5521dfe17c8b119d58577b5777e8e1a
dstrobl <Dominic.Strobl@bam.de>
2022-11-08 14:49:39 +0100

    update  convection test, but still don't work

--- tests/integration/test_convection2D.py
+++ tests/integration/test_convection2D.py
@@ -13,6 +13,7 @@ import numpy as np
 
 from pgdrome.solver import PGDProblem, FD_matrices
 
+
 def create_meshes(num_elem, ord, ranges):
 
     meshes = list()
@@ -27,9 +28,10 @@ def create_meshes(num_elem, ord, ranges):
         meshes.append(mesh_tmp)
         Vs.append(Vs_tmp)
 
-    return meshes, Vs #x,y,t,h
+    return meshes, Vs  # x,y,t,h
 
-def create_bc(Vs,dom,param):
+
+def create_bc(Vs, dom, param):
     # boundary conditions list
 
     # Initial condition
@@ -38,10 +40,10 @@ def create_bc(Vs,dom,param):
 
     initCond = dolfin.DirichletBC(Vs[2], 0, init)
 
-    return [0, 0, initCond, 0] #x,y,t,h
+    return [0, 0, initCond, 0]  # x,y,t,h
 
 
-def create_doms_pgd(Vs,param):
+def create_doms_pgd(Vs, param):
     boundary_x = dolfin.MeshFunction("size_t", Vs[0].mesh(), Vs[0].mesh().topology().dim() - 1)
     boundary_x.set_all(0)
 
@@ -50,15 +52,15 @@ def create_doms_pgd(Vs,param):
 
     class zero(dolfin.SubDomain):
         def inside(self, x, on_boundary):
-            return on_boundary and dolfin.near(x[0], 0., dolfin.DOLFIN_EPS)
+            return on_boundary and dolfin.near(x, 0., dolfin.DOLFIN_EPS)
 
     class ly(dolfin.SubDomain):
         def inside(self, x, on_boundary):
-            return on_boundary and dolfin.near(x[0], param['ly']/param['L_0'], dolfin.DOLFIN_EPS)
+            return on_boundary and np.isclose(x, np.max(Vs[1].mesh().coordinates()[:]), dolfin.DOLFIN_EPS)
 
     class lx(dolfin.SubDomain):
         def inside(self, x, on_boundary):
-            return on_boundary and dolfin.near(x[0], param['lx']/param['L_0'], dolfin.DOLFIN_EPS)
+            return on_boundary and np.isclose(x, np.max(Vs[0].mesh().coordinates()[:]), dolfin.DOLFIN_EPS)
 
     Zero_x = zero()
     Zero_x.mark(boundary_x, 1)
@@ -70,9 +72,10 @@ def create_doms_pgd(Vs,param):
     LY = ly()
     LY.mark(boundary_y, 2)
 
-    return [boundary_x,boundary_y,0,0] # x,y,t,h
+    return [boundary_x, boundary_y, 0, 0]  # x,y,t,h
 
-def problem_assemble_lhs_FDtime(fct_F,var_F,Fs,meshes,dom,param,typ,dim):
+
+def problem_assemble_lhs_FDtime(fct_F ,var_F, Fs, meshes, dom, param, typ, dim):
     # problem discription left hand side of DGL for each fixed point problem
     ds_x = dolfin.Measure('ds', domain=meshes[0], subdomain_data=dom[0])
     ds_y = dolfin.Measure('ds', domain=meshes[1], subdomain_data=dom[1])
@@ -88,7 +91,7 @@ def problem_assemble_lhs_FDtime(fct_F,var_F,Fs,meshes,dom,param,typ,dim):
     alpha_h1 = dolfin.assemble(Fs[3] * Fs[3] * dolfin.dx(meshes[3]))
     alpha_h2 = dolfin.assemble(Fs[3] * param['h_fct'] * Fs[3] * dolfin.dx(meshes[3]))
 
-    if typ == 'r': #x
+    if typ == 'r':  # x
         a = dolfin.Constant(alpha_y1 * alpha_t1 * alpha_h1) * param['a1'] * param["rho"] * param["cp"] \
             * fct_F * var_F * dolfin.dx(meshes[0]) \
             + dolfin.Constant(alpha_y1 * alpha_t2 * alpha_h1) * param['a2'] * param["k"] \
@@ -100,30 +103,31 @@ def problem_assemble_lhs_FDtime(fct_F,var_F,Fs,meshes,dom,param,typ,dim):
             + dolfin.Constant(dolfin.assemble(Fs[1]*Fs[1]*ds[1](2)) * alpha_t2 * alpha_h2) * param['h1']\
             * fct_F * var_F * dolfin.dx(meshes[0])
 
-    if typ == 's': #y
+    if typ == 's':  # y
         a = dolfin.Constant(alpha_x1 * alpha_t1 * alpha_h1) * param['a1'] * param["rho"] * param["cp"] \
             * fct_F * var_F * dolfin.dx(meshes[1]) \
             + dolfin.Constant(alpha_x2 * alpha_t2 * alpha_h1) * param['a2'] * param["k"]\
             * fct_F * var_F * dolfin.dx(meshes[1]) \
-            + dolfin.Constant(alpha_x1 * alpha_t2  * alpha_h1) * param['a2'] * param["k"]\
+            + dolfin.Constant(alpha_x1 * alpha_t2 * alpha_h1) * param['a2'] * param["k"]\
             * fct_F.dx(0) * var_F.dx(0) * dolfin.dx(meshes[1]) \
             + dolfin.Constant(dolfin.assemble(Fs[0]*Fs[0]*ds[0](2)) * alpha_t2 * alpha_h2) * param['h1']\
             * fct_F * var_F * dolfin.dx(meshes[1])\
             + dolfin.Constant(alpha_x1 * alpha_t2 * alpha_h2) * param['h1']\
             * fct_F * var_F * ds[1](2)
 
-    if typ == 'v': #t
+    if typ == 'v':  # t
         a = alpha_x1 * alpha_y1 * alpha_h1 * param['a1'] * param["rho"] * param["cp"] * param['D1_up_t'] \
             + alpha_x2 * alpha_y1 * alpha_h1 * param['a2'] * param["k"] * param['M_t'] \
             + alpha_x1 * alpha_y2 * alpha_h1 * param['a2'] * param["k"] * param['M_t'] \
             + dolfin.assemble(Fs[0]*Fs[0]*ds[0](2)) * alpha_y1 * alpha_h2 * param['h1'] * param['M_t'] \
             + alpha_x1 * dolfin.assemble(Fs[1] * Fs[1] * ds[1](2)) * alpha_h2 * param['h1'] * param['M_t']
-            # add initial condition
+        # add initial condition
+        a = a.tolil()
         a[:, param['bc_idx']] = 0
         a[param['bc_idx'], :] = 0
         a[param['bc_idx'], param['bc_idx']] = 1
 
-    if typ == 'w': #h
+    if typ == 'w':  # h
         a = dolfin.Constant(alpha_x1 * alpha_y1 * alpha_t1) * param['a1'] * param["rho"] * param["cp"]\
             * fct_F * var_F * dolfin.dx(meshes[3]) \
             + dolfin.Constant(alpha_x2 * alpha_y1 * alpha_t2) * param['a2'] * param["k"]\
@@ -148,8 +152,8 @@ def problem_assemble_rhs_FDtime(fct_F,var_F,Fs,meshes,dom,param,Q,PGD_func,typ,n
     ds = [ds_x, ds_y]
 
     # print('----CHECK BOUNDARY----')
-    # print('A1', dolfin.assemble(1 * ds[1](2)) * dolfin.assemble(1 * dolfin.dx(meshes[0]))) #dx*dy
-    # print('A2', dolfin.assemble(1 * ds[0](2)) * dolfin.assemble(1 * dolfin.dx(meshes[1]))) #dx*dy
+    # print('A1', dolfin.assemble(1 * ds[1](2)) * dolfin.assemble(1 * dolfin.dx(meshes[0])))  # dx*dy
+    # print('A2', dolfin.assemble(1 * ds[0](2)) * dolfin.assemble(1 * dolfin.dx(meshes[1])))  # dx*dy
     # print('full A', dolfin.assemble(1 * dolfin.dx(meshes[1])) * dolfin.assemble(1 * dolfin.dx(meshes[0])))
     # summarize IC and PGD_func[old]
     Told = []
@@ -168,7 +172,7 @@ def problem_assemble_rhs_FDtime(fct_F,var_F,Fs,meshes,dom,param,Q,PGD_func,typ,n
     beta_t2 = Fs[2].vector()[:].transpose() @ param['M_t'] @ Tamb[2].vector()[:]
     beta_h2 = dolfin.assemble(Tamb[3] * param['h_fct'] * Fs[3] * dolfin.dx(meshes[3]))
 
-    if typ == 'r': #x
+    if typ == 'r':  # x
         l = dolfin.Constant(beta_y1 * beta_t2 * beta_h2) * param['h1'] \
             * Tamb[0] * var_F * ds[0](2) \
             + dolfin.Constant(dolfin.assemble(Tamb[1] * Fs[1] * ds[1](2)) * beta_t2 * beta_h2) * param['h1'] \
@@ -192,7 +196,7 @@ def problem_assemble_rhs_FDtime(fct_F,var_F,Fs,meshes,dom,param,Q,PGD_func,typ,n
                 - dolfin.Constant(dolfin.assemble(Fs[1] * Fs[1] * ds[1](2)) * alpha_old_t2 * alpha_old_h2) * param['h1'] \
                 * Told[0][old] * var_F * dolfin.dx(meshes[0])
 
-    if typ == 's': #y
+    if typ == 's':  # y
         l = dolfin.Constant(dolfin.assemble(Tamb[0] * Fs[0] * ds[0](2)) * beta_t2 * beta_h2) * param['h1']\
             * Tamb[1] * var_F * dolfin.dx(meshes[1]) \
             + dolfin.Constant(beta_x1 * beta_t2 * beta_h2) * param['h1']\
@@ -208,14 +212,14 @@ def problem_assemble_rhs_FDtime(fct_F,var_F,Fs,meshes,dom,param,Q,PGD_func,typ,n
                 * Told[1][old] * var_F * dolfin.dx(meshes[1]) \
                 - dolfin.Constant(alpha_old_x2 * alpha_old_t2 * alpha_old_h1) * param['a2'] * param["k"]\
                 * Told[1][old] * var_F * dolfin.dx(meshes[1]) \
-                - dolfin.Constant(alpha_old_x1 * alpha_old_t2  * alpha_old_h1) * param['a2'] * param["k"]\
+                - dolfin.Constant(alpha_old_x1 * alpha_old_t2 * alpha_old_h1) * param['a2'] * param["k"]\
                 * Told[1][old].dx(0) * var_F.dx(0) * dolfin.dx(meshes[1]) \
                 - dolfin.Constant(dolfin.assemble(Fs[0]*Fs[0]*ds[0](2)) * alpha_old_t2 * alpha_old_h2) * param['h1']\
                 * Told[1][old] * var_F * dolfin.dx(meshes[1])\
                 - dolfin.Constant(alpha_old_x1 * alpha_old_t2 * alpha_old_h2) * param['h1']\
                 * Told[1][old] * var_F * ds[1](2)
 
-    if typ == 'v': #t
+    if typ == 'v':  # t
         l = dolfin.assemble(Tamb[0] * Fs[0] * ds[0](2)) * beta_y1 * beta_h2 * param['h1'] * param['M_t'] @ Tamb[2].vector()[:] \
             + beta_x1 * dolfin.assemble(Tamb[1] * Fs[1] * ds[1](2)) * beta_h2 * param['h1'] * param['M_t'] @ Tamb[2].vector()[:]
 
@@ -236,12 +240,12 @@ def problem_assemble_rhs_FDtime(fct_F,var_F,Fs,meshes,dom,param,Q,PGD_func,typ,n
                 - dolfin.assemble(Fs[0] * Fs[0] * ds[0](2)) * alpha_old_y1 * alpha_old_h2 * param['h1']\
                 * param['M_t'] @ Told[2][old].vector()[:]\
                 - alpha_old_x1 * dolfin.assemble(Fs[1] * Fs[1] * ds[1](2)) * alpha_old_h2 * param['h1']\
-                * param['M_t']@ Told[2][old].vector()[:]
+                * param['M_t'] @ Told[2][old].vector()[:]
 
         # add initial condition
         l[param['bc_idx']] = 0
 
-    if typ == 'w': #h
+    if typ == 'w':  # h
         l = dolfin.Constant(dolfin.assemble(Tamb[0] * Fs[0] * ds[0](2)) * beta_y1 * beta_t2) * param['h1']\
             * Tamb[3] * param['h_fct'] * var_F * dolfin.dx(meshes[3]) \
             + dolfin.Constant(beta_x1 * dolfin.assemble(Tamb[1] * Fs[1] * ds[1](2)) * beta_t2) * param['h1']\
@@ -315,7 +319,8 @@ def create_PGD(param={}, vs=[]):
 
     return pgd_s, param
 
-def create_doms_fem(v,param):
+
+def create_doms_fem(v, param):
     boundary = dolfin.MeshFunction("size_t", v.mesh(), v.mesh().topology().dim()-1)
     boundary.set_all(0)
 
@@ -346,6 +351,7 @@ def create_doms_fem(v,param):
 
     return boundary
 
+
 # reference model FEM in space and backward euler in time
 class Reference:
 
@@ -368,7 +374,7 @@ class Reference:
         self.h = dolfin.Constant(1.)
 
         self.F = self.param['a1'] * self.param["rho"] * self.param["cp"] * T * v * dolfin.dx() \
-                 + self.param['a2'] * self.dt * self.param["k"] *  dolfin.dot(dolfin.grad(T), dolfin.grad(v)) * dolfin.dx() \
+                 + self.param['a2'] * self.dt * self.param["k"] * dolfin.dot(dolfin.grad(T), dolfin.grad(v)) * dolfin.dx() \
                  + self.dt * self.param['h1'] * self.h * (T - self.T_amb) * v * ds(2) \
                  + self.dt * self.param['h1'] * self.h * (T - self.T_amb) * v * ds(4) \
                  - self.param['a1'] * self.param["rho"] * self.param["cp"] * self.T_n * v * dolfin.dx()
@@ -386,7 +392,7 @@ class Reference:
         # check time mesh for requested time value
         if not np.where(self.time_mesh == values[0])[0]:
             print("ERROR time step not in mesh What to do?")
-        self.h.assign(values[1]) # * self.param["h0"]) #?
+        self.h.assign(values[1])  # * self.param["h0"]) #?
 
         # Time-stepping
         Ttime = []
@@ -410,7 +416,6 @@ class Reference:
             Ttime.append(Ttmp)
             Tpoint.append(np.copy(T(self.point)))
 
-
         return Ttime, Tpoint  # solution in time over xy and time solution at fixed point (x,y)
 
 
@@ -421,28 +426,28 @@ class problem(unittest.TestCase):
         # global parameters
         self.param = {"rho": 7100., "cp": 3100., "k": 100., 'Tamb': 25., 'T_ic': 500.,
                       'lx': 0.15, 'ly': 0.1, 'lt': 20.,
-                      'L_0': 1, 't_0': 1., 'T_0': 1.} # possiblity to make the equation dimless
-                      #'L_0': 0.1, 't_0': 100., 'T_0': 500. }
+                      'L_0': 1, 't_0': 1., 'T_0': 1.}
+                      #'L_0': 0.1, 't_0': 100., 'T_0': 500. }  # possibility to make the equation dimless
 
         # factors for pde
         self.param['a1'] = self.param['T_0'] / self.param['t_0']
         self.param['a2'] = self.param['T_0'] / self.param['L_0'] ** 2.
         self.param['h1'] = self.param['T_0'] / self.param['L_0']
 
-        self.ranges = [[0., self.param['lx']/ self.param['L_0']],  # xmin, xmax
-                       [0., self.param['ly']/ self.param['L_0']],  # ymin, ymax
-                       [0., self.param['lt']/ self.param['t_0']],  # tmin, tmax
+        self.ranges = [[0., self.param['lx'] / self.param['L_0']],  # xmin, xmax
+                       [0., self.param['ly'] / self.param['L_0']],  # ymin, ymax
+                       [0., self.param['lt'] / self.param['t_0']],  # tmin, tmax
                        [40., 150.]]  # hmin, hmax
 
         self.ords = [1, 1, 1, 1]  # x, y, t, h
         self.femorder = 2
-        # self.elems = [20, 10, 20, 10]
-        self.elems = [80, 50, 20, 100]
+        self.elems = [20, 10, 20, 10]
+        # self.elems = [80, 50, 20, 100]
         # self.elems = [200, 100, 20, 100]
 
         self.h_fixed = [50, 100]
         self.point = (self.param['lx'] / self.param['L_0'], self.param['ly']/(2*self.param['L_0']))
-        self.fixed_dim = 2 # time
+        self.fixed_dim = 2  # time
 
         self.plotting = True
         # self.plotting = False
@@ -462,13 +467,13 @@ class problem(unittest.TestCase):
                                                T=self.param["T_ic"] / self.param['T_0'])  # initial condition FEM
         self.param['IC_h'] = dolfin.Expression('1.0', degree=1)
 
-        self.param['h_fct'] = dolfin.Expression('x[0]',degree=1)
+        self.param['h_fct'] = dolfin.Expression('x[0]', degree=1)
 
         meshes, vs = create_meshes(self.elems, self.ords, self.ranges)
         pgd_fd, param = create_PGD(param=self.param, vs=vs)
 
         # evaluation at one point with fixed h
-        u_pgd_01 = pgd_fd.evaluate(self.fixed_dim, [0,1,3], [self.point[0], self.point[1], self.h_fixed[0]], 0)
+        u_pgd_01 = pgd_fd.evaluate(self.fixed_dim, [0, 1, 3], [self.point[0], self.point[1], self.h_fixed[0]], 0)
         upgd_bc_01 = u_pgd_01.compute_vertex_values()[:] + \
                      param['IC_x'](self.point[0]) * param['IC_y'](self.point[1]) * param['IC_t'].compute_vertex_values()[:] * param["IC_h"](
             self.h_fixed[0])
@@ -485,37 +490,36 @@ class problem(unittest.TestCase):
         # reconstruct x-y field for given h and last time step
         X = np.array(meshes[0].coordinates()[:]).flatten()
         Y = np.array(meshes[1].coordinates()[:]).flatten()
-        Z = np.ones((len(Y),len(X)))
+        Z = np.ones((len(Y), len(X)))
         for idx_x, xi in enumerate(X):
             for idx_y, yi in enumerate(Y):
                 u_tmp = pgd_fd.evaluate(self.fixed_dim, [0, 1, 3], [xi, yi, self.h_fixed[0]], 0)
                 uu_tmp = u_tmp.compute_vertex_values()[:] + \
                           param['IC_x'](xi) * param['IC_y'](yi) \
                           * param['IC_t'].compute_vertex_values()[:] * param["IC_h"](self.h_fixed[0])
-                Z[idx_y,idx_x] = self.param['T_0'] * uu_tmp[-1]
+                Z[idx_y, idx_x] = self.param['T_0'] * uu_tmp[-1]
 
         # FEM
         self.param['IC_t'] = dolfin.Expression('T', degree=1, T=self.param["T_ic"]/self.param['T_0'])  # initial condition FEM
 
-        mesh_xy = dolfin.RectangleMesh(dolfin.Point(self.ranges[0][0],self.ranges[1][0]),dolfin.Point(self.ranges[0][1],self.ranges[1][1]), self.elems[0], self.elems[1])
-        v_xy = dolfin.FunctionSpace(mesh_xy,'CG',self.femorder)
+        mesh_xy = dolfin.RectangleMesh(dolfin.Point(self.ranges[0][0], self.ranges[1][0]), dolfin.Point(self.ranges[0][1], self.ranges[1][1]), self.elems[0], self.elems[1])
+        v_xy = dolfin.FunctionSpace(mesh_xy, 'CG', self.femorder)
         v_t = vs[2]
-        doms_fem = create_doms_fem(v_xy,self.param)
+        doms_fem = create_doms_fem(v_xy, self.param)
 
-        u_fem, u_fem2_01 = Reference(param=self.param, vs=[v_xy, v_t], dom = doms_fem, point=self.point)(
+        u_fem, u_fem2_01 = Reference(param=self.param, vs=[v_xy, v_t], dom=doms_fem, point=self.point)(
             [self.param['lt'], self.h_fixed[0]])
-        print('FEM',np.array(u_fem2_01)*self.param['T_0'])
+        print('FEM', np.array(u_fem2_01)*self.param['T_0'])
 
         _, u_fem2_02 = Reference(param=self.param, vs=[v_xy, v_t], dom=doms_fem, point=self.point)(
             [self.param['lt'], self.h_fixed[1]])
         print('FEM', np.array(u_fem2_02) * self.param['T_0'])
 
-
         if self.plotting:
             import matplotlib.pyplot as plt
 
             plt.figure(1)
-            plt.plot(meshes[2].coordinates()[:]* self.param['t_0'], np.array(u_fem2_01)*self.param['T_0'], '-or', label='FEM h1')
+            plt.plot(meshes[2].coordinates()[:] * self.param['t_0'], np.array(u_fem2_01)*self.param['T_0'], '-or', label='FEM h1')
             plt.plot(meshes[2].coordinates()[:] * self.param['t_0'], np.array(u_fem2_02) * self.param['T_0'], '-or',
                      label='FEM h2')
             plt.plot(meshes[2].coordinates()[:] * self.param['t_0'], upgd_bc_01 * self.param['T_0'],
@@ -534,18 +538,18 @@ class problem(unittest.TestCase):
             plt.ylabel('y')
             plt.title('FEM Temp end time')
 
-            fig, ax = plt.subplots(1,1) #, figsize=(15,15), dpi=80)
-            X, Y = np.meshgrid(X,Y)
+            fig, ax = plt.subplots(1, 1) #, figsize=(15,15), dpi=80)
+            X, Y = np.meshgrid(X, Y)
             cp = ax.contourf(X, Y, Z)
             fig.colorbar(cp)  # Add a colorbar to a plot
             ax.set_title('PGD Temp end time')
             ax.set_xlabel('x')
             ax.set_ylabel('y')
 
-            fig, axall = plt.subplots(2,2)
-            ax=axall[0,0]
+            fig, axall = plt.subplots(2, 2)
+            ax = axall[0, 0]
             for i in range(pgd_fd.used_numModes):
-                ax.plot(meshes[0].coordinates()[:]*self.param['L_0'],pgd_fd.mesh[0].attributes[0].interpolationfct[i].compute_vertex_values()[:])
+                ax.plot(meshes[0].coordinates()[:]*self.param['L_0'], pgd_fd.mesh[0].attributes[0].interpolationfct[i].compute_vertex_values()[:])
             ax.set_title('X modes')
 
             ax = axall[0, 1]
@@ -568,8 +572,8 @@ class problem(unittest.TestCase):
 
             plt.show()
 
-
         self.assertTrue(0.00001 < 1e-3)
+
 
 if __name__ == '__main__':
     dolfin.set_log_level(dolfin.LogLevel.ERROR)
