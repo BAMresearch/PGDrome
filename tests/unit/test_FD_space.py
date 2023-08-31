6b477978ea1aeb2926b84f16e0deede2a847ee74
dstrobl <Dominic.Strobl@bam.de>
2023-08-31 09:04:09 +0200

    applied black code style to upload

--- tests/unit/test_FD_space.py
+++ tests/unit/test_FD_space.py
@@ -40,7 +40,7 @@ class FD_solution_dirichlet:
         IC[0] = self.param["T_amb"]
 
         # set up shorted heat equation
-        Amat = - D2
+        Amat = -D2
         Fvec = D2 @ IC
 
         # set matrices for initial condition
@@ -86,23 +86,23 @@ class FD_solution_robin:
         Q = dolfin.interpolate(self.q, self.Vs).vector()[:]
 
         # set up shorted heat equation
-        Amat = - D2
+        Amat = -D2
         Fvec = M @ Q
 
         # define space step size
-        dx = (x_dofs[0] - x_dofs[1])
+        dx = x_dofs[0] - x_dofs[1]
 
         # set matrices for initial condition
         Amat[:, 0] = 0
         Amat[0, :] = 0
         Amat[0, 0] = 1 / dx + self.param["h"]
-        Amat[0, 1] = - 1 / dx
-        Amat[1, 0] = - 1 / dx
+        Amat[0, 1] = -1 / dx
+        Amat[1, 0] = -1 / dx
         Amat[:, -1] = 0
         Amat[-1, :] = 0
         Amat[-1, -1] = 1 / dx + self.param["h"]
-        Amat[-1, -2] = - 1 / dx
-        Amat[-2, -1] = - 1 / dx
+        Amat[-1, -2] = -1 / dx
+        Amat[-2, -1] = -1 / dx
 
         Fvec[0] = self.param["h"] * self.param["T_amb"]
         Fvec[-1] = self.param["h"] * self.param["T_amb"]
@@ -118,7 +118,6 @@ class FD_solution_robin:
 
 class FEM_solution_dirichlet:
     def __init__(self, Vs=None, param=None, meshes=None, q=None):
-
         self.Vs = Vs  # Location
         # self.Vs = dolfin.FunctionSpace(Vs.mesh(), 'CG', 2)  # to improve solution
         self.param = param  # Parameters
@@ -126,23 +125,24 @@ class FEM_solution_dirichlet:
         self.q = q  # heat source expression
 
     def run(self):
-
         # boundary condition
         def left(x, on_boundary):
-            return x < np.min(self.Vs.mesh().coordinates()[:]) + 1E-8
+            return x < np.min(self.Vs.mesh().coordinates()[:]) + 1e-8
 
         def right(x, on_boundary):
-            return x > np.max(self.Vs.mesh().coordinates()[:]) - 1E-8
+            return x > np.max(self.Vs.mesh().coordinates()[:]) - 1e-8
 
-        bc = [dolfin.DirichletBC(self.Vs, 100, left),
-              dolfin.DirichletBC(self.Vs, self.param["T_amb"], right)]
+        bc = [
+            dolfin.DirichletBC(self.Vs, 100, left),
+            dolfin.DirichletBC(self.Vs, self.param["T_amb"], right),
+        ]
 
         # set up problem
         T = fenics.TrialFunction(self.Vs)
         v = fenics.TestFunction(self.Vs)
 
         a = fenics.dot(fenics.grad(T), fenics.grad(v)) * fenics.dx()
-        l = dolfin.Expression('0', degree=1) * v * dolfin.dx
+        l = dolfin.Expression("0", degree=1) * v * dolfin.dx
 
         # solve problem
         T = fenics.Function(self.Vs)
@@ -160,12 +160,13 @@ class FEM_solution_robin:
         self.q = q  # heat source expression
 
     def run(self):
-
         # Assign boundary conditions
-        boundary_parts = fenics.MeshFunction("size_t", self.meshes, self.meshes.topology().dim() - 1)
+        boundary_parts = fenics.MeshFunction(
+            "size_t", self.meshes, self.meshes.topology().dim() - 1
+        )
         boundary_parts.set_all(0)  # marks whole domain as 0
 
-        tol = 1E-8
+        tol = 1e-8
 
         l_bc_point = np.min(self.Vs.mesh().coordinates()[:])
         r_bc_point = np.max(self.Vs.mesh().coordinates()[:])
@@ -183,19 +184,23 @@ class FEM_solution_robin:
         right = Right()
         right.mark(boundary_parts, 2)
 
-        ds = fenics.Measure('ds', domain=self.meshes, subdomain_data=boundary_parts)
+        ds = fenics.Measure("ds", domain=self.meshes, subdomain_data=boundary_parts)
 
         # set up problem
         T = fenics.TrialFunction(self.Vs)
         v = fenics.TestFunction(self.Vs)
 
         T_amb_func = fenics.Function(self.Vs)
-        T_amb_func.vector()[:] = self.param['T_amb'] * np.ones(len(T_amb_func.vector()[:]))
+        T_amb_func.vector()[:] = self.param["T_amb"] * np.ones(
+            len(T_amb_func.vector()[:])
+        )
 
-        F = fenics.dot(fenics.grad(T), fenics.grad(v)) * fenics.dx() \
-            - self.q * v * fenics.dx() \
-            + self.param['h'] * (T - T_amb_func) * v * ds(1) \
-            + self.param['h'] * (T - T_amb_func) * v * ds(2)
+        F = (
+            fenics.dot(fenics.grad(T), fenics.grad(v)) * fenics.dx()
+            - self.q * v * fenics.dx()
+            + self.param["h"] * (T - T_amb_func) * v * ds(1)
+            + self.param["h"] * (T - T_amb_func) * v * ds(2)
+        )
         a, l = fenics.lhs(F), fenics.rhs(F)
 
         # solve problem
@@ -216,7 +221,7 @@ class PGDproblem(unittest.TestCase):
         self.param = {
             "T_amb": 25,
             "h": 15,
-        }  # material density [kg/m³]  # heat conductivity [W/m°C] # specific heat capacity [J/kg°C]
+        }  # ambient temperature in °C  # heat transfer coefficient in W/(m²°C)
 
         self.plotting = True
         # self.plotting = False
@@ -229,16 +234,24 @@ class PGDproblem(unittest.TestCase):
         Vs_x = dolfin.FunctionSpace(mesh_x, "CG", self.ord)
         q = dolfin.Constant(1.0)
 
-        TFD_dirichlet = FD_solution_dirichlet(Vs=Vs_x, meshes=mesh_x, param=self.param, q=q).run()
+        TFD_dirichlet = FD_solution_dirichlet(
+            Vs=Vs_x, meshes=mesh_x, param=self.param, q=q
+        ).run()
         # print(TFD_dirichlet.compute_vertex_values()[:])
 
-        TFD_robin = FD_solution_robin(Vs=Vs_x, meshes=mesh_x, param=self.param, q=q).run()
+        TFD_robin = FD_solution_robin(
+            Vs=Vs_x, meshes=mesh_x, param=self.param, q=q
+        ).run()
         # print(TFD_robin.compute_vertex_values()[:])
 
-        TFEM_dirichlet = FEM_solution_dirichlet(Vs=Vs_x, meshes=mesh_x, param=self.param, q=q).run()
+        TFEM_dirichlet = FEM_solution_dirichlet(
+            Vs=Vs_x, meshes=mesh_x, param=self.param, q=q
+        ).run()
         # print(TFEM_dirichlet.compute_vertex_values()[:])
 
-        TFEM_robin = FEM_solution_robin(Vs=Vs_x, meshes=mesh_x, param=self.param, q=q).run()
+        TFEM_robin = FEM_solution_robin(
+            Vs=Vs_x, meshes=mesh_x, param=self.param, q=q
+        ).run()
         # print(TFEM_robin.compute_vertex_values()[:])
 
         # check errors
@@ -249,7 +262,9 @@ class PGDproblem(unittest.TestCase):
         print("error robin bc FD - FEM:", error_robin)
 
         self.assertTrue(error_dirichlet < 1e-8)
-        self.assertTrue(error_robin < 1e-1)  # expect up to single percent error for robin bc
+        self.assertTrue(
+            error_robin < 1e-1
+        )  # expect up to single percent error for robin bc
 
         if self.plotting:
             import matplotlib.pyplot as plt
@@ -269,9 +284,9 @@ class PGDproblem(unittest.TestCase):
                 label="FEM",
             )
             plt.legend()
-            plt.title('Dirichlet boundary condition')
-            plt.xlabel('space x')
-            plt.ylabel('Temperature T in °C')
+            plt.title("Dirichlet boundary condition")
+            plt.xlabel("space x")
+            plt.ylabel("Temperature T in °C")
 
             # robin bc
             plt.figure()
@@ -288,23 +303,27 @@ class PGDproblem(unittest.TestCase):
                 label="FEM",
             )
             plt.legend()
-            plt.title('Robin boundary condition')
-            plt.xlabel('space x')
-            plt.ylabel('Temperature T in °C')
+            plt.title("Robin boundary condition")
+            plt.xlabel("space x")
+            plt.ylabel("Temperature T in °C")
 
             # error robin bc
             plt.figure()
             plt.plot(
                 mesh_x.coordinates()[:],
-                100 * (TFEM_robin.compute_vertex_values()[:] - TFD_robin.compute_vertex_values()[:]) /
-                TFEM_robin.compute_vertex_values()[:],
+                100
+                * (
+                    TFEM_robin.compute_vertex_values()[:]
+                    - TFD_robin.compute_vertex_values()[:]
+                )
+                / TFEM_robin.compute_vertex_values()[:],
                 "-*b",
-                label="error FD- FEM",
+                label="error FD - FEM",
             )
             plt.legend()
-            plt.title('Relative error for robin boundary condition')
-            plt.xlabel('space x')
-            plt.ylabel('relative error in %')
+            plt.title("Relative error for robin boundary condition")
+            plt.xlabel("space x")
+            plt.ylabel("relative error in %")
 
             plt.show()
 
